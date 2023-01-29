717e2a0a23e8a1fc3be1c87499860d6e6a6713f8
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-01-29 12:24:31 +0500

    task manager add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import './App.css'
-import RouterIndex from "./routes/index"
+import RouterIndex from './routes/index'
 import Header from './UI/Header'
 import Footer from './UI/Footer'
+import context from 'react-bootstrap/esm/AccordionContext'
+
 function App() {
+  const [check, setCheck] = useState(false)
+  const [time, setTime] = useState([])
+  const [inputWork, setInputWork] = useState('')
+  const [inputTime, setInputTime] = useState('')
   return (
     <>
-      <Header/>
-      <RouterIndex />
-      <Footer/>
+      <context.Provider
+        value={{
+
+          time,
+          setTime,
+          inputWork,
+          setInputWork,
+          inputTime,
+          setInputTime,
+          check,
+          setCheck,
+        }}
+      >
+        <Header />
+        <RouterIndex />
+        <Footer />
+      </context.Provider>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,98 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import context from 'react-bootstrap/esm/AccordionContext'
 
 const Home = () => {
+    const { inputWork, setInputWork, inputTime, setInputTime, check, setCheck } =
+        useContext(context);
+    const [ sd, setSd] = useState([])
+    let tasks = [];
+    let workarr = [];
+  function handlerAdd() {
+    const task = {
+      work: inputWork,
+      time: inputTime,
+    }
+    tasks.push(task)
+    localStorage.setItem('work', JSON.stringify(tasks))
+  }
+    function getWork() {
+        JSON.parse(localStorage.getItem('work')).map((t) => setSd([t,...sd]))
+  }
   return (
     <>
       <div className='container'>
-        <div className='d-flex'>
-          <input
-            type='text'
-            placeholder='what do you do'
-            id=''
-            name=''
-            value={''}
-            className='form-control'
-          />
-          <input
-            type='text'
-            placeholder='how much time do you have?'
-            id=''
-            name=''
-            value={''}
-            className='form-control'
-          />
-          <input
-            type='text'
-            placeholder=''
-            id=''
-            name=''
-            value={''}
-            className='form-control'
-          />
-          <input
-            type='text'
-            placeholder=''
-            id=''
-            name=''
-            value={''}
-            className='form-control'
-          />
-          <input
-            type='text'
-            placeholder=''
-            id=''
-            name=''
-            value={''}
-            className='form-control'
-          />
+        <div className='row'>
+          <div className='col-lg-6 col-md-6 col-sm-12'>
+            <input
+              type='text'
+              placeholder='what do you do'
+              id=''
+              name=''
+              value={inputWork}
+              onChange={(e) => {
+                setInputWork(e.target.value)
+              }}
+              className='form-control text-center'
+            />
+          </div>
+          <div className='col-lg-6 col-md-6 col-sm-12'>
+            <input
+              type='text'
+              placeholder='how much time do you have?'
+              id=''
+              name=''
+              value={inputTime}
+              onChange={(e) => {
+                setInputTime(e.target.value)
+              }}
+              className='form-control text-center'
+            />
+          </div>
+          <div className='col-12 mt-2 '>
+            <button
+              className='btn btn-info w-100 text-light'
+              onClick={handlerAdd}
+            >
+              add
+            </button>
+          </div>
         </div>
+        {check ? (
+          ''
+        ) : (
+          <button
+            className='btn btn-primary w-100 mt-5'
+            onClick={() => {
+                setCheck(true);
+                getWork()
+            }}
+          >
+            start my works
+          </button>
+        )}
+        {check ? (
+          <div className='row mt-5 border p-3'>
+            <div className='col-4'>
+              <h2>your work</h2>
+              <ul>
+                <li>Work 1</li>
+              </ul>
+            </div>
+            <div className='col-4'>
+              <h2>your time</h2>
+              <ul>
+                <li>time 1</li>
+              </ul>
+            </div>
+            <div className='col-4'>
+              <h2>time limet</h2>
+              <button>start</button>
+              <button>pause</button>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     </>
   )
