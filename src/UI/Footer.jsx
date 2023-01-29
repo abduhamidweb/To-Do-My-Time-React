714e8b8280fd96d35b4543e5c5dc4bba7f767522
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-01-29 13:11:38 +0500

    finished

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -1,11 +1,129 @@
-import React from 'react';
+import React from 'react'
 
 const Footer = () => {
-    return (
-        <>
-         <h1>Footer</h1>   
-        </>
-    );
-};
+  return (
+    <>
+      <footer class='text-center text-lg-start bg-white text-muted footer'>
+        <section class='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+          <div class='me-5 d-none d-lg-block'>
+            <span>Get connected with us on social networks:</span>
+          </div>
+          <div>
+            <a href='' class='me-4 link-secondary'>
+              <i class='fab fa-facebook-f'></i>
+            </a>
+            <a href='' class='me-4 link-secondary'>
+              <i class='fab fa-twitter'></i>
+            </a>
+            <a href='' class='me-4 link-secondary'>
+              <i class='fab fa-google'></i>
+            </a>
+            <a href='' class='me-4 link-secondary'>
+              <i class='fab fa-instagram'></i>
+            </a>
+            <a href='' class='me-4 link-secondary'>
+              <i class='fab fa-linkedin'></i>
+            </a>
+            <a href='' class='me-4 link-secondary'>
+              <i class='fab fa-github'></i>
+            </a>
+          </div>
+        </section>
 
-export default Footer;
+        <section class=''>
+          <div class='container text-center text-md-start mt-5'>
+            <div class='row mt-3'>
+              <div class='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+                <h6 class='text-uppercase fw-bold mb-4'>
+                  <i class='fas fa-gem me-3 text-secondary'></i>Company name
+                </h6>
+                <p>
+                  Here you can use rows and columns to organize your footer
+                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+
+              <div class='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+                <h6 class='text-uppercase fw-bold mb-4'>Products</h6>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Angular
+                  </a>
+                </p>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    React
+                  </a>
+                </p>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Vue
+                  </a>
+                </p>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Laravel
+                  </a>
+                </p>
+              </div>
+
+              <div class='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+                <h6 class='text-uppercase fw-bold mb-4'>Useful links</h6>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Pricing
+                  </a>
+                </p>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Settings
+                  </a>
+                </p>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Orders
+                  </a>
+                </p>
+                <p>
+                  <a href='#!' class='text-reset'>
+                    Help
+                  </a>
+                </p>
+              </div>
+
+              <div class='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+                <h6 class='text-uppercase fw-bold mb-4'>Contact</h6>
+                <p>
+                  <i class='fas fa-home me-3 text-secondary'></i> New York, NY
+                  10012, US
+                </p>
+                <p>
+                  <i class='fas fa-envelope me-3 text-secondary'></i>
+                  info@example.com
+                </p>
+                <p>
+                  <i class='fas fa-phone me-3 text-secondary'></i> + 01 234 567
+                  88
+                </p>
+                <p>
+                  <i class='fas fa-print me-3 text-secondary'></i> + 01 234 567
+                  89
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div class='text-center p-4'>
+          © 2021 Copyright:
+          <a class='text-reset fw-bold' href='https://mdbootstrap.com/'>
+            MDBootstrap.com
+          </a>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,98 +2,115 @@ import React, { useContext, useState } from 'react'
 import context from 'react-bootstrap/esm/AccordionContext'
 
 const Home = () => {
-    const { inputWork, setInputWork, inputTime, setInputTime, check, setCheck } =
-        useContext(context);
-    const [ sd, setSd] = useState([])
-    let tasks = [];
-    let workarr = [];
+  const { inputWork, setInputWork, inputTime, setInputTime, check, setCheck } =
+    useContext(context)
+  const [sd, setSd] = useState(false)
+  const [ass, setAss] = useState('finshied')
+  const [clas, setclas] = useState(' btn btn-danger w-100 mt-2')
+  let tasks = []
   function handlerAdd() {
     const task = {
       work: inputWork,
       time: inputTime,
     }
     tasks.push(task)
-    localStorage.setItem('work', JSON.stringify(tasks))
-  }
-    function getWork() {
-        JSON.parse(localStorage.getItem('work')).map((t) => setSd([t,...sd]))
+
+    localStorage.setItem('work', JSON.stringify(tasks));
   }
   return (
     <>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-6 col-md-6 col-sm-12'>
-            <input
-              type='text'
-              placeholder='what do you do'
-              id=''
-              name=''
-              value={inputWork}
-              onChange={(e) => {
-                setInputWork(e.target.value)
-              }}
-              className='form-control text-center'
-            />
+      <main>
+        <div className='container mt-5'>
+          <div className='row'>
+            <div className='col-lg-6 col-md-6 col-sm-12'>
+              <input
+                type='text'
+                placeholder='what do you do'
+                id=''
+                name=''
+                value={inputWork}
+                onChange={(e) => {
+                  setInputWork(e.target.value)
+                }}
+                className='form-control text-center'
+              />
+            </div>
+            <div className='col-lg-6 col-md-6 col-sm-12'>
+              <input
+                type='text'
+                placeholder='how much time do you have?'
+                id=''
+                name=''
+                value={inputTime}
+                onChange={(e) => {
+                  setInputTime(e.target.value)
+                }}
+                className='form-control text-center'
+              />
+            </div>
+            <div className='col-12 mt-2 '>
+              <button
+                className='btn btn-info w-100 text-light'
+                onClick={() => {
+                  handlerAdd()
+                  setSd(true)
+                }}
+              >
+                add
+              </button>
+            </div>
           </div>
-          <div className='col-lg-6 col-md-6 col-sm-12'>
-            <input
-              type='text'
-              placeholder='how much time do you have?'
-              id=''
-              name=''
-              value={inputTime}
-              onChange={(e) => {
-                setInputTime(e.target.value)
-              }}
-              className='form-control text-center'
-            />
-          </div>
-          <div className='col-12 mt-2 '>
+          {sd ? (
             <button
-              className='btn btn-info w-100 text-light'
-              onClick={handlerAdd}
+              className={clas}
+              onClick={() => {
+                setCheck(true)
+                setAss('oke')
+                setclas(' btn btn-success w-100 mt-2')
+              }}
             >
-              add
+              {ass}
             </button>
-          </div>
+          ) : (
+            ''
+          )}
+          {check ? (
+            <div className='row mt-5 border p-3'>
+              <div className='col-4'>
+                <h2>your work</h2>
+              </div>
+              <div className='col-4'>
+                <h2>your time</h2>
+              </div>
+              <div className='col-4'>
+                <h2>time limet</h2>
+              </div>
+              {JSON.parse(localStorage.getItem('work')).map((item) => {
+                return (
+                  <>
+                    <div className='col-4'>
+                      <ul>
+                        <li>{item.work ? item.work : 'any'}</li>
+                      </ul>
+                    </div>
+                    <div className='col-4'>
+                      <ul>
+                        <li>{item.time ? item.time : 'any'}</li>
+                      </ul>
+                    </div>
+                    <div className='col-4'>
+                      <button>start</button>
+                      <button>pause</button>
+                    </div>
+                  </>
+                )
+              })}
+            </div>
+          ) : (
+            ''
+          )}
         </div>
-        {check ? (
-          ''
-        ) : (
-          <button
-            className='btn btn-primary w-100 mt-5'
-            onClick={() => {
-                setCheck(true);
-                getWork()
-            }}
-          >
-            start my works
-          </button>
-        )}
-        {check ? (
-          <div className='row mt-5 border p-3'>
-            <div className='col-4'>
-              <h2>your work</h2>
-              <ul>
-                <li>Work 1</li>
-              </ul>
-            </div>
-            <div className='col-4'>
-              <h2>your time</h2>
-              <ul>
-                <li>time 1</li>
-              </ul>
-            </div>
-            <div className='col-4'>
-              <h2>time limet</h2>
-              <button>start</button>
-              <button>pause</button>
-            </div>
-          </div>
-        ) : (
-          ''
-        )}
-      </div>
+      </main>
     </>
   )
 }
